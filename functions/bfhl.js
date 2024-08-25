c972a8fc2e406cb1e89eebb54758403618866d49
Mahata <91476640+mahatalakshmi@users.noreply.github.com>
2024-08-25 11:51:49 +0530

    Update bfhl.js

--- functions/bfhl.js
+++ functions/bfhl.js
@@ -1,54 +1,39 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
+const dotenv = require('dotenv')
+const Pipedrive = require('pipedrive')
 
-const app = express();
-app.use(bodyParser.json());
+const defaultClient = Pipedrive.ApiClient.instance;
+let apiToken = defaultClient.authentications.api_key;
+apiToken.apiKey = process.env.PIPEDRIVE_API_KEY;
 
-const router = express.Router();
+exports.handler = async function (event, context) {
+    const { firstName, lastName, email, phone } = JSON.parse(event.body)
 
-router.post('/', (req, res) => {
-    const { data, full_name, dob, email, roll_number } = req.body;
-
-    if (!full_name || !dob || !email || !roll_number || !Array.isArray(data)) {
-        return res.status(400).json({
-            is_success: false,
-            message: 'Invalid input',
+    try {
+        const personApi = new Pipedrive.PersonsApi();
+        let personOpts = Pipedrive.NewPerson.constructFromObject({
+            name: firstName + ' ' + lastName,
+            phone: [{ "value": phone, "primary": "true", "label": "main" }],
+            email: [{ "value": email, "primary": "true", "label": "mobile" }]
         });
+        const { data } = await personApi.addPerson(personOpts)
+        const person = data
+        const leadsApi = new Pipedrive.LeadsApi();
+        let leadsOpts = Pipedrive.AddLeadRequest.constructFromObject({
+            title: `${person.name}'s lead`,
+            personId: person.id,
+        });
+        const record = await leadsApi.addLead(leadsOpts)
+        return {
+            statusCode: 201,
+            body: JSON.stringify(record)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Server Error',
+        };
     }
 
-    const user_id = `${full_name.replace(/\s+/g, '_')}_${dob.replace(/-/g, '')}`;
 
-    const numbers = [];
-    const alphabets = [];
-    let highest_lowercase = '';
 
-    data.forEach(item => {
-        if (!isNaN(item)) {
-            numbers.push(item);
-        } else if (typeof item === 'string') {
-            alphabets.push(item);
-            if (item === item.toLowerCase() && item > highest_lowercase) {
-                highest_lowercase = item;
-            }
-        }
-    });
-
-    res.json({
-        is_success: true,
-        user_id: user_id,
-        email: email,
-        roll_number: roll_number,
-        numbers: numbers,
-        alphabets: alphabets,
-        highest_lowercase_alphabet: highest_lowercase ? [highest_lowercase] : []
-    });
-});
-
-router.get('/', (req, res) => {
-    res.status(200).json({ operation_code: 1 });
-});
-
-app.use('/.netlify/functions/bfhl', router);
-
-module.exports.handler = serverless(app);
+};
